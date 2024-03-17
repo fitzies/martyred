fc5bf1e5885b797e565cfd6ae14c46fd10e72b25
Mr Bushido <olicrypt@gmail.com>
2024-03-17 19:23:18 +0800

    fixing nav pt2

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,18 +1,25 @@
+import { IoPersonOutline } from "react-icons/io5";
+
 const Nav = () => {
   let items = ["NEW ARRIVALS", "TOPS", "PANTS", "ACCESSORIES"];
   return (
-    <div className="w-screen flex justify-between px-10 py-8">
-      <div className="">logo</div>
-      <div className="flex gap-8 lg:block">
-        {items.map((item) => {
+    <div className="w-screen justify-between px-10 py-8 flex">
+      <div className="lg:flex hidden">logo</div>
+      <div className="gap-8 flex">
+        {items.map((item, index) => {
           return (
-            <div className="cursor-pointer" key={item}>
+            <div
+              className={`${
+                index === 0 ? "lg:flex hidden" : ""
+              } cursor-pointer`}
+              key={item}
+            >
               {item}
             </div>
           );
         })}
       </div>
-      <div>Account</div>
+      <IoPersonOutline />
     </div>
   );
 };
